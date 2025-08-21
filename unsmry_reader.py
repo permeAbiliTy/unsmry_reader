e6af558e17de144793ca919f72be76b148d482d6
Adam Turner <133763036+permeAbiliTy@users.noreply.github.com>
2025-08-21 11:42:54 +0100

    Update unsmry_reader.py with GPLv3 notice
    Added GPLv3 notice to source code file

--- unsmry_reader.py
+++ unsmry_reader.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""    
+Extracts data from Slb Eclipse binary summary files (*.UNSMRY) into a pandas dataframe
+    Copyright (C) 2025  Adam Turner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 # Used to convert bytes to double
 import struct
 
